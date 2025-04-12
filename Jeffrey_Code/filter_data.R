ccdc244c010bd18d7b3233461570b892141132a2
Jeffrey Gong <jeffrey.gong@emory.edu>
2025-04-12 04:45:02 -0400

    initial commit

---
+++ Jeffrey_Code/filter_data.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(here)
+
+here::i_am("Jeffrey_Code/filter_data.R")
+
+AllData = read.csv(here("Data/nba_2025-03-07"))
+
+#Create conference column
+
+# Named vector mapping team abbreviations to conferences
+conference_map <- c(
+  ATL = "East", BOS = "East", BKN = "East", CHA = "East", CHI = "East", CLE = "East",
+  DET = "East", IND = "East", MIA = "East", MIL = "East", NYK = "East", ORL = "East",
+  PHI = "East", TOR = "East", WAS = "East",
+  
+  DAL = "West", DEN = "West", GSW = "West", HOU = "West", LAC = "West", LAL = "West",
+  MEM = "West", MIN = "West", NOP = "West", OKC = "West", PHX = "West", POR = "West",
+  SAC = "West", SAS = "West", UTA = "West"
+)
+
+# Add a conference column
+AllData = AllData |>
+  mutate(conference = conference_map[Team])
+
+#Test:
+#Sys.setenv(WHICH_CONFIG="one_team")
+
+WHICH_CONFIG = Sys.getenv("WHICH_CONFIG")
+
+config_list = config::get(config=WHICH_CONFIG)
+
+#Filter for specified conference
+if(config_list$conference %in% c("West","East")) {
+  AllData = AllData |> filter(conference==config_list$conference)
+}
+
+#Filter for specified team
+if(config_list$team == "CUSTOM"){
+  TEAM = Sys.getenv("TEAM")
+  AllData = AllData |> filter(Team==TEAM)
+}
+
+#Filter for specified position
+if(config_list$position == "CUSTOM"){
+  POS = Sys.getenv("POS")
+  AllData = AllData |> filter(Pos==POSITION)
+}
+
+saveRDS(AllData,here("Clean_Data/data.rds"))
+
+
---
+++ README.md
@@ -0,0 +1,29 @@
+Data 550 Midterm Project
+================
+Group 2
+
+# Configuring the Report
+
+In the terminal, use
+
+> export WHICH_CONFIG = “…”
+
+to choose one of the following configurations:
+
+1.  default: Analysis will include all players
+2.  west: Analysis includes only Western Conference Players
+3.  east: Analysis includes only Eastern Conference Players
+4.  one_team: Analysis includes only a specified team. Specify the team
+    in the terminal with
+
+> export TEAM = “…”
+
+replacing … with the three letter abbreviation of the desired team.
+
+5.  one_position: Analysis includes only players of a specified
+    position. Specify the position in the terminal with
+
+> export POS = “…”
+
+replacing … with the abbreviation of the desired position(C, PG, SG, PF,
+SF)
