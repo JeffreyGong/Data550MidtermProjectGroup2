79617f511af508bb8c50cce476efe4e8245a18d2
Joseph Lee <josephlee0918@gmail.com>
2025-04-15 19:19:00 -0400

    Joseph: Add PTS vs. 3PTA regression analysis

---
+++ Joseph_Code/three_pt_reg.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+library(here)
+library(gtsummary)
+
+here::i_am("Joseph_Code/three_pt_reg.R")
+
+Data <- readRDS(here("Clean_Data/data.rds"))
+
+# Filter out players who attempted less than 10 total three-pointers
+filtered_data <- Data %>%
+  filter(X3PA >= 10)
+
+# regression: Total Points (PTS) as a function of Three-Point Attempts (X3PA)
+reg_model <- lm(PTS ~ X3PA, data = filtered_data)
+
+summary(reg_model)
+
+# Creat regression table
+reg_table <- tbl_regression(reg_model)
+
+saveRDS(reg_table, here::here("Outputs/3pts_reg_table.rds"))
+
+# Create scatter plot w/ regression line (Each point represents one player's season data)
+scatterplot <- ggplot(filtered_data, aes(x = X3PA, y = PTS)) +
+  geom_point(color = "darkgreen") +      
+  geom_smooth(method = "lm", se = FALSE, color = "blue") +
+  labs(
+    title = "Regression Analysis: Three-Point Attempts vs. Total Points",
+    x = "Three-Point Attempts",
+    y = "Total Points"
+  ) +
+  theme_light()
+
+ggsave(
+  here::here("Outputs/three_pt_regression_plot.png"),
+  plot = scatterplot,
+  device = "png"
+)
+
